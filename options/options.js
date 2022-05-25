d65ca6a48c4650fbcf087735a3c3ebe53d15baf3
Crowdbotics <team@crowdbotics.com>
2022-05-25 14:20:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "kinanthropometry_35429",
+  url: "https://kinanthropometry_35429.botics.co",
+  api: "https://kinanthropometry_35429.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
